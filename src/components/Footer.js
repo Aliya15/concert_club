6a2487adaee158172bb0649c2939dd5498bfe50b
Aliya15 <aliyuwe4ka@gmail.com>
2022-05-28 15:49:15 +0300

    added styles and jsonplaceholder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import Button from '@mui/material/Button';
 
 export default function Footer() {
     return (
-        <header className='footer'>
+        <footer className='footer'>
             <h2 className='footer_title'>О группе</h2>
             <p>Twenty One Pilots — американский дуэт из Колумбуса, штат Огайо. Группа образовалась в 2009 году и
                 на данный момент состоит из Тайлера Джозефа и Джоша Дана.
                 Коллектив самостоятельно выпустил два альбома: Twenty One Pilots в 2009 и Regional at Best в 2011.</p>
-        </header>
+        </footer>
     )
 }
--- src/components/ProfilesList.js
+++ src/components/ProfilesList.js
@@ -1,7 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import {useEffect, useState} from "react";
+import './ProfilesList.scss'
 
 export default function ProfilesList() {
+    const [profiles, setProfiles] = useState();
+
+    const getProfilesList = async () => {
+        const list = await fetch('https://my-json-server.typicode.com/aliya15/concert_club/profilesList')
+        const listToJson = await list.json();
+        await setProfiles(listToJson);
+        console.log(listToJson)
+    }
+
+    useEffect(() => {
+        getProfilesList().then();
+        console.log(profiles)
+    })
 
     return (
         <div className='profilesList'>
@@ -10,10 +25,19 @@ export default function ProfilesList() {
                 <p>932/1000</p>
             </div>
             <div className='profilesList_profiles'>
-                <h4>NAME</h4>
-                <p>CITY</p>
-                <Button>Смотреть профиль</Button>
+                {
+                    profiles?.map((item) => {
+                        return (
+                            <div className='profilesList_profiles_item'>
+                                <h4>{item.name}</h4>
+                                <p>{item.city}</p>
+                                <Button>Смотреть профиль</Button>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
 }
+
